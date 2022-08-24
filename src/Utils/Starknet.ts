7396e1d84b333d7ef95a1e0fdb4670288836c84b
Uğur Eren <contact@ugureren.net>
2022-08-25 00:36:59 +0300

    initialize contract

--- src/Utils/Starknet.ts
+++ src/Utils/Starknet.ts
@@ -1,7 +1,14 @@
-import {RpcProvider} from 'starknet';
+import {Contract, defaultProvider, RpcProvider} from 'starknet';
+import EarlystarkersABI from '../ABIs/EarlystarkersAbi.json';
 
-const {STARKNET_NODE_URL} = process.env;
+const {STARKNET_NODE_URL, CONTRACT_ADDRESS} = process.env;
 
 export const provider = new RpcProvider({
   nodeUrl: STARKNET_NODE_URL || 'https://alpha4.starknet.io',
 });
+
+export const contract = new Contract(
+  EarlystarkersABI as any,
+  CONTRACT_ADDRESS || '',
+  defaultProvider,
+);
