add8afa18f9510b4415973edafed0d77cc2135b9
Uğur Eren <contact@ugureren.net>
2022-08-30 21:17:19 +0300

    Show logs

--- src/CRON.ts
+++ src/CRON.ts
@@ -7,6 +7,8 @@ const INTERVAL_IN_SECONDS = Number(process.env.CRON_INTERVAL_IN_SECONDS) || 60;
 let failedPreviousRun = false;
 
 const CRONJob = async (): Promise<void> => {
+  console.info(`CRON job started at ${new Date()}`);
+
   try {
     const stars = await getAllStarsInfo();
 
@@ -20,9 +22,16 @@ const CRONJob = async (): Promise<void> => {
     store.dispatch(StarsActions.replaceStars(mappedStars));
   } catch (_) {
     // If failed again, don't try again for a while
-    if (failedPreviousRun) return;
+    if (failedPreviousRun) {
+      console.warn('CRON job failed again, not trying again for a while');
+
+      failedPreviousRun = false;
+      return;
+    }
 
     failedPreviousRun = true;
+
+    console.warn('CRON job failed, trying again in 10 seconds');
 
     // retry after 10 seconds
     await new Promise((resolve) => {
@@ -31,6 +40,8 @@ const CRONJob = async (): Promise<void> => {
 
     await CRONJob();
   }
+
+  console.info(`CRON job completed at ${new Date()}`);
 };
 
 // Run the CRON job when started
--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,5 @@ app.use('/', router);
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.info(`Express server started listening on port ${PORT}`);
 });
