cf07057cd4634098f8c8d1c9e61fa6c56914679d
Hikmet Can Köseoğlu <hikmetcanizmir@gmail.com>
2022-09-01 22:45:39 +0300

    wl check

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import Stars from './Routes/Stars';
 import Metadata from './Routes/Metadata';
+import Wl from "./Routes/Wl";
 
 const Router = express.Router();
 
 Router.use('/stars', Stars);
 Router.use('/', Metadata);
+Router.use('/wl', Wl);
 
 export default Router;
